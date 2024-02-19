64f1e62927af1a87a7b835aaa7af9a321cdb7565
Paul Ohrt <paul.ohrt@internetstiftelsen.se>
2024-02-19 02:01:34 +0100

    refactor: remove unused Stats methods

--- stats/stats.go
+++ stats/stats.go
@@ -14,26 +14,10 @@ func (s *Stats) MemUsedKb() uint64 {
 	return s.MemStats.MemTotal - s.MemStats.MemAvailable
 }
 
-func (s *Stats) MemUsedPercent() uint64 {
-	return s.MemStats.MemAvailable / s.MemStats.MemTotal
-}
-
-func (s *Stats) MemAvailableKb() uint64 {
-	return s.MemStats.MemAvailable
-}
-
 func (s *Stats) MemTotalKb() uint64 {
 	return s.MemStats.MemTotal
 }
 
 func (s *Stats) DiskTotal() uint64 {
 	return s.DiskStats.All
-}
-
-func (s *Stats) DiskFree() uint64 {
-	return s.DiskStats.Free
-}
-
-func (s *Stats) DiskUsed() uint64 {
-	return s.DiskStats.Used
 }
